cdb8db4e8c854b591368894c6cf44b2eff4272e0
ZK-2O <90663909+ZK-2O@users.noreply.github.com>
2021-09-14 21:07:46 -0600

    Exercise - Parts 1 and 2

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -28,16 +28,92 @@ Blockchain.blocks.push({
 });
 
 // TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
+for (let line of poem) {
+	createBlock(line)
+}
 
-// console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
+function createBlock(data) {
+	let block = {
+		index: Blockchain.blocks.length,
+		prevHash: blockHash(Blockchain.blocks[Blockchain.blocks.length - 1]),
+		data: data,
+		timestamp: Date.now() 
+	}
 
+	block.hash = blockHash(block) //Because we need to hash the data for the block, we do it separately from the bit above
+	Blockchain.blocks.push(block)
+	
+	console.log(block)
+	return block
+}
 
 // **********************************
 
 function blockHash(bl) {
+	var stringJSON = JSON.stringify(bl)
 	return crypto.createHash("sha256").update(
-		// TODO: use block data to calculate hash
+		stringJSON
 	).digest("hex");
 }
+
+//Test
+
+function verifyChain(chain) {
+	for (var i = 0; i < chain.blocks.length; i++)
+	{
+		if (!verifyBlock(chain.blocks[i]))
+		{
+			console.log("Chain invalid!")
+			return false;
+		}
+	}
+
+	console.log("Chain valid!");
+	return true;
+}
+
+function verifyBlock(bl) {
+	//Check if Genesis Block
+	if (isEmpty(bl.data) &&
+		bl.index === 0 &&
+		bl.hash === "000000")
+	{
+		//If these conditions meet, it's a valid genesis block
+		return true;
+	}
+
+	let blockInfo = {
+		index: bl.index,
+		prevHash: bl.prevHash,
+		data: bl.data,
+		timestamp: bl.timestamp
+	};
+
+	//For the rest of the blocks
+	if (isEmpty(bl.data) ||
+		isNaN(parseInt(bl.index)) || 
+		parseInt(bl.index) < 0 ||
+		isEmpty(bl.prevHash) ||
+		bl.hash != blockHash(blockInfo))
+	{
+		//If any of the above conditions are met, it's an INVALID block
+		console.log(`Invalid Block:\r\n${JSON.stringify(bl)}`)
+		return false;
+	}
+
+	return true;
+}
+
+function isEmpty(val) {
+	if (!val || val === null || val === undefined || val.length === 0) {
+		return true;
+	}
+
+	return false;
+}
+
+//Uncomment below to simulate invalid block
+Blockchain.blocks[3].data = ""
+
+let chain = verifyChain(Blockchain)
+console.log(chain)
